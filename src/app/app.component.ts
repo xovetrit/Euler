8075e0db010eefacf9e48a2ee1520a06e8fca6d2
Abylay <auzakbaev20@gmail.com>
2020-10-30 12:59:45 +0500

    Задача 1 - Числа, кратные 3 или 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Euler';
+  
+  sum = 0;
+
+  constructor() {
+
+    for (let i = 0; i < 1000; i++) 
+      if (i % 3 == 0 || i % 5 == 0)  this.sum += i;
+    
+  }
 }
