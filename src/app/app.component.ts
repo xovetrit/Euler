78a6c02b9a283ebb11bfd6edceb81dd8975fca73
Abylay <auzakbaev20@gmail.com>
2021-01-14 11:18:52 +0500

    Задача 7 - 10001-ое простое число

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,43 +10,42 @@ export class AppComponent {
   otvet = 0;
 
   code = `
-     var summa10 = 0;
+    var arr = []
+    var index = 0;
 
-      var summaKvadratov = 0;
-      var kvadratSummy = 0;
-      var raznost = 0;
-
-      for ( var x = 1; x <= 100; x++) {
-        summaKvadratov += x ** 2
-        summa10 += x
-      } 
-
-      kvadratSummy = summa10 ** 2
-      raznost = kvadratSummy - summaKvadratov
+    for ( var x = 1; x <= 150000; x++ ) {
+      var count = 0;
+      for ( var y = 1; y <= x; y++ ) {
+        if ( x % y === 0 ) {
+          count++;
+        }
+      }
+      if ( count === 2 ) {
+        arr[index++] = x
+      }
+      
+    }
+    console.log(arr[10000])
   `
   constructor(){
 
-    var summa10 = 0;
+    var arr = []
+    var index = 0;
 
-    var summaKvadratov = 0;
-    var kvadratSummy = 0;
-    var raznost = 0;
-
-    for ( var x = 1; x <= 100; x++) {
-      summaKvadratov += x ** 2
-      summa10 += x
-    } 
-
-    kvadratSummy = summa10 ** 2
-    raznost = kvadratSummy - summaKvadratov
-
-    this.otvet = raznost
-
-    // console.log("Сумма квадратов: " + summaKvadratov)
-    // console.log("Квадрат суммы: " +  kvadratSummy)
-    // console.log("Разность: " + raznost)
-    
-
+    for ( var x = 1; x <= 150000; x++ ) {
+      var count = 0;
+      for ( var y = 1; y <= x; y++ ) {
+        if ( x % y === 0 ) {
+          count++;
+        }
+      }
+      if ( count === 2 ) {
+        arr[index++] = x
+      }
+      
+    }
+    console.log(arr[10000])//10001-1 => потому что с нуля считаем индекс
+    this.otvet = arr[10000]
   }
 
 }
