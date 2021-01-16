263bfe06b36233de0c47161593e7b45e6d6d2e49
Abylay <auzakbaev20@gmail.com>
2021-01-16 14:22:00 +0500

    Задача 10 - Сложение простых чисел

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,48 +6,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  otvet = [];
-  otvet2 = 0;
-  otvet3 = 0;
+  otvet = 0;
 
   code = `
-  for ( var a = 1; a < 1000; a++ ) {
-    for ( var b = a + 1; b < 1000; b++ ) {
-      var cbefore = Math.sqrt(a ** 2 + b ** 2)
-      var c = 0;
-      if ( cbefore % 1 === 0) {
-        c = cbefore;
-      }
-      if ( b < c && a < c ) {
-        var sum = a + b + c;
-        if  ( sum === 1000 ) {
-          console.log(a + " + " + b + " + " + c + " = " + sum + " a * b * c = " + a*b*c)
-        }
+  var sum = 0;    
+  for ( var i = 1; i <= 2000; i++ ) {
+    var count = 0;
+    for ( var y = 1; y <= i; y++) {
+      if ( i % y === 0 ) {
+        count++;
       }
     }
+    if ( count === 2 ) {
+      sum += i
+    }
   }
+  console.log(sum)
+ 
   `
   constructor(){
-
-    for ( var a = 1; a < 1000; a++ ) {
-      for ( var b = a + 1; b < 1000; b++ ) {
-        var cbefore = Math.sqrt(a ** 2 + b ** 2)
-        var c = 0;
-        if ( cbefore % 1 === 0) {
-          c = cbefore;
-        }
-        if ( b < c && a < c ) {
-          var sum = a + b + c;
-          if  ( sum === 1000 ) {
-            console.log(a + " + " + b + " + " + c + " = " + sum + " a * b * c = " + a*b*c)
-            this.otvet = [a, b , c]
-            this.otvet2 = sum;
-            this.otvet3 = a * b * c;
-          }
+    
+    var sum = 0;    
+    for ( var i = 1; i <= 2000; i++ ) {
+      var count = 0;
+      for ( var y = 1; y <= i; y++) {
+        if ( i % y === 0 ) {
+          count++;
         }
       }
+      if ( count === 2 ) {
+        sum += i
+      }
     }
+    console.log(sum)
+    this.otvet = sum
   }
+
 
 }
 
