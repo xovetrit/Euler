a28ac4e55bfbca7acdf0db8eb6927a317860ae4a
Abylay <auzakbaev20@gmail.com>
2021-01-18 13:54:26 +0500

    Задача 12 - Треугольное число с большим количеством делителей

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,145 +9,53 @@ export class AppComponent {
   otvet = 0;
 
   code = `
-  var arr = [
-    [8, 2, 22, 97, ... ],
-    [49, 49, 99, ... ]
-    ...
-  ]
+  var arr = [];
+  var index = 0;
+  var sum = 0;
 
-  var sum4horizontal = 0;
-  var arr4horizontal = [];
+  for ( var x = 1; x <= 13000; x++ ) {
+    arr[index++] = x + sum;
+    sum += x;
+  }
 
-  for ( var i = 0; i < arr.length; i++ ) {
-    for ( var y = 0; y < arr.length; y++ ) {
-      if ( arr[i][y] * arr[i][y + 1] * arr[i][y + 2] * arr[i][y + 3] > sum4horizontal ) {
-        sum4horizontal = arr[i][y] * arr[i][y + 1] * arr[i][y + 2] * arr[i][y + 3]
-        arr4horizontal = [arr[i][y], arr[i][y + 1], arr[i][y + 2], arr[i][y + 3]]
+  for ( var y = 0; y <= arr.length; y++ ) {
+    let count = 0;
+    for ( var z = 1; z <= arr[y]; z++) {
+      if ( arr[y] % z === 0 ) {
+        count++;
+      }
+      if ( count > 500) {
+        console.log(arr[y])
+        break;
       }
     }
   }
-
-  var sum4vertical = 0;
-  var arr4vertical = [];
-
-  for ( var w = 0; w < arr.length; w++ ) {
-    for ( var q = 0; q < arr.length - 3; q++ ) {
-      if ( arr[q][w] * arr[q + 1][w] * arr[q + 2][w] * arr[q + 3][w] > sum4vertical ) {
-        sum4vertical = arr[q][w] * arr[q + 1][w] * arr[q + 2][w] * arr[q + 3][w]
-        arr4vertical = [arr[q][w], arr[q + 1][w], arr[q + 2][w], arr[q + 3][w]]
-      }
-    }
-  }
-
-  var sum4diagonalSE = 0;
-  var arr4diagonalSE = [];
-
-  for ( var z = 0; z < arr.length - 3; z++ ) {
-    for ( var j = 0; j < arr.length - 3; j++ ) {
-      if ( arr[z][j] * arr[z + 1][j + 1] * arr[z + 2][j + 2] * arr[z + 3][j + 3] > sum4diagonalSE ) {
-        sum4diagonalSE = arr[z][j] * arr[z + 1][j + 1] * arr[z + 2][j + 2] * arr[z + 3][j + 3]
-        arr4diagonalSE = [arr[z][j], arr[z + 1][j + 1], arr[z + 2][j + 2], arr[z + 3][j + 3]]
-      }
-    }
-  }
-
-  var sum4diagonalSW = 0;
-  var arr4diagonalSW = [];
-
-  for ( var x = 0; x < arr.length - 3; x++ ) {
-    for (var l = 19; l > 0; l-- ) {
-      if ( arr[x][l] * arr[x + 1][l - 1] * arr[x + 2][l - 2] * arr[x + 3][l - 3] > sum4diagonalSW) {
-        sum4diagonalSW = arr[x][l] * arr[x + 1][l - 1] * arr[x + 2][l - 2] * arr[x + 3][l - 3];
-        arr4diagonalSW = [arr[x][l], arr[x + 1][l - 1], arr[x + 2][l - 2], arr[x + 3][l - 3]]
-      }
-    }
-  }
-
-  var sum4Max = Math.max(sum4horizontal, sum4vertical, sum4diagonalSE, sum4diagonalSW);
-  console.log("Max: " + sum4Max);
   `
   constructor(){
     
-    var arr = [
-      [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-      [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-      [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-      [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-      [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-      [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-      [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-      [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-      [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-      [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-      [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-      [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-      [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-      [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-      [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-      [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-      [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-      [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-      [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-      [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-    ]
+    var arr = [];
+    var index = 0;
+    var sum = 0;
 
-    var sum4horizontal = 0;
-    var arr4horizontal = [];
+    for ( var x = 1; x <= 13000; x++ ) {
+      arr[index++] = x + sum;
+      sum += x;
+    }
 
-    for ( var i = 0; i < arr.length; i++ ) {
-      for ( var y = 0; y < arr.length; y++ ) {
-        if ( arr[i][y] * arr[i][y + 1] * arr[i][y + 2] * arr[i][y + 3] > sum4horizontal ) {
-          sum4horizontal = arr[i][y] * arr[i][y + 1] * arr[i][y + 2] * arr[i][y + 3]
-          arr4horizontal = [arr[i][y], arr[i][y + 1], arr[i][y + 2], arr[i][y + 3]]
+    for ( var y = 0; y <= arr.length; y++ ) {
+      let count = 0;
+      for ( var z = 1; z <= arr[y]; z++) {
+        if ( arr[y] % z === 0 ) {
+          count++;
+        }
+        if ( count > 500) {
+          console.log(arr[y])
+          this.otvet = arr[y]
+          break;
         }
       }
     }
 
-    var sum4vertical = 0;
-    var arr4vertical = [];
-
-    for ( var w = 0; w < arr.length; w++ ) {
-      for ( var q = 0; q < arr.length - 3; q++ ) {
-        if ( arr[q][w] * arr[q + 1][w] * arr[q + 2][w] * arr[q + 3][w] > sum4vertical ) {
-          sum4vertical = arr[q][w] * arr[q + 1][w] * arr[q + 2][w] * arr[q + 3][w]
-          arr4vertical = [arr[q][w], arr[q + 1][w], arr[q + 2][w], arr[q + 3][w]]
-        }
-      }
-    }
-
-    var sum4diagonalSE = 0;
-    var arr4diagonalSE = [];
-
-    for ( var z = 0; z < arr.length - 3; z++ ) {
-      for ( var j = 0; j < arr.length - 3; j++ ) {
-        if ( arr[z][j] * arr[z + 1][j + 1] * arr[z + 2][j + 2] * arr[z + 3][j + 3] > sum4diagonalSE ) {
-          sum4diagonalSE = arr[z][j] * arr[z + 1][j + 1] * arr[z + 2][j + 2] * arr[z + 3][j + 3]
-          arr4diagonalSE = [arr[z][j], arr[z + 1][j + 1], arr[z + 2][j + 2], arr[z + 3][j + 3]]
-        }
-      }
-    }
-
-    var sum4diagonalSW = 0;
-    var arr4diagonalSW = [];
-
-    for ( var x = 0; x < arr.length - 3; x++ ) {
-      for (var l = 19; l > 0; l-- ) {
-        if ( arr[x][l] * arr[x + 1][l - 1] * arr[x + 2][l - 2] * arr[x + 3][l - 3] > sum4diagonalSW) {
-          sum4diagonalSW = arr[x][l] * arr[x + 1][l - 1] * arr[x + 2][l - 2] * arr[x + 3][l - 3];
-          arr4diagonalSW = [arr[x][l], arr[x + 1][l - 1], arr[x + 2][l - 2], arr[x + 3][l - 3]]
-        }
-      }
-    }
-
-    var sum4Max = Math.max(sum4horizontal, sum4vertical, sum4diagonalSE, sum4diagonalSW);
-
-    console.log("Horizontal: " + sum4horizontal, arr4horizontal)
-    console.log("Vertical: " + sum4vertical, arr4vertical)
-    console.log("Diagonal South-East: " + sum4diagonalSE, arr4diagonalSE)
-    console.log("Diagonal South-West: " + sum4diagonalSW, arr4diagonalSW)
-    
-    console.log("Max: " + sum4Max);
-    this.otvet = sum4Max;
   }
 
 
